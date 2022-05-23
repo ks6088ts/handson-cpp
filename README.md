23e1b47c95968283c0cb99f66d53e9a877d199e9
ks6088ts <ks6088ts@gmail.com>
2022-05-23 15:24:42 +0900

    add ci test

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+[![test](https://github.com/ks6088ts/handson-cpp/workflows/test/badge.svg)](https://github.com/ks6088ts/handson-cpp/actions/workflows/test.yml)
+
 # handson-cpp
+
 handson for C++
--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
+// Copyright © 2022 ks6088ts. All rights reserved.
 #include <iostream>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
-}
+int main(int, char **) { std::cout << "Hello, world!\n"; }
